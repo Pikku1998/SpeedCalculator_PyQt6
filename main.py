d13e32829b7899de519e4464494af4376013c890
Prakash R <prakashramanukolanu1998@gmail.com>
2023-08-23 16:04:48 +0530

    Calculate speed

--- main.py
+++ main.py
@@ -5,40 +5,52 @@ import sys
 class SpeedCalculator(QWidget):
     def __init__(self):
         super().__init__()
-        self.resize(300, 250)
+        # self.resize(400, 300)
         self.setWindowTitle('Speed Calculator')
 
         # Create type of layout
         grid = QGridLayout()
 
         # Create Widgets (labels, line_edits and push_buttons)
-        distance_label = QLabel('Distance: ')
+        distance_label = QLabel('Distance(in km): ')
         self.distance_line_edit = QLineEdit()
 
-        self.distance_unit = QComboBox()
-        self.distance_unit.addItems(['kilometers', 'miles'])
-
-        time_label = QLabel('Time: ')
+        time_label = QLabel('Time(in hours): ')
         self.time_line_edit = QLineEdit()
 
-        button = QPushButton("Calculate Speed")
-        button.clicked.connect(self.calculate_speed)
+        speed_units_label = QLabel('Speed in units: ')
+        self.speed_unit = QComboBox()
+        self.speed_unit.addItems(['km/h', 'mph'])
+
+        calculate_button = QPushButton("Calculate Speed")
+        calculate_button.clicked.connect(self.calculate_speed)
         self.output_label = QLabel("")
 
         # Add Widgets to grid layout
         grid.addWidget(distance_label, 0, 0)
         grid.addWidget(self.distance_line_edit, 0, 1)
-        grid.addWidget(self.distance_unit, 0, 2)
         grid.addWidget(time_label, 1, 0)
         grid.addWidget(self.time_line_edit, 1, 1)
-        grid.addWidget(button, 2, 1, 1, 1)
-        grid.addWidget(self.output_label, 3, 0, 1, 2)
+        grid.addWidget(speed_units_label, 2, 0)
+        grid.addWidget(self.speed_unit, 2, 1)
+        grid.addWidget(calculate_button, 3, 0, 1, 1)
+        grid.addWidget(self.output_label, 4, 0)
 
         # Set Layout to grid
         self.setLayout(grid)
 
     def calculate_speed(self):
-        self.output_label.setText('Clicked!')
+        distance = float(self.distance_line_edit.text())
+        time = float(self.time_line_edit.text())
+        speed = distance/time
+
+        if self.speed_unit.currentText() == 'km/h':
+            speed = round(speed, 2)
+            units = 'km/h'
+        if self.speed_unit.currentText() == 'mph':
+            speed = round(speed * 0.621371, 2)
+            units = 'mph'
+        self.output_label.setText(f"{speed} {units}")
 
 
 app = QApplication(sys.argv)
